5f7116bb3a4b07db444a04cda02ef40dd52efb3a
Kimonas Sotirchos <kimonas.sotirchos@canonical.com>
2025-10-07 17:34:09 +0300

    chore: update module path for poddefaults
    Signed-off-by: Kimonas Sotirchos <kimonas.sotirchos@canonical.com>

--- components/poddefaults-webhooks/pkg/apis/settings/v1alpha1/register.go
+++ components/poddefaults-webhooks/pkg/apis/settings/v1alpha1/register.go
@@ -18,7 +18,7 @@ limitations under the License.
 // Package v1alpha1 contains API Schema definitions for the settings v1alpha1 API group
 // +k8s:openapi-gen=true
 // +k8s:deepcopy-gen=package,register
-// +k8s:conversion-gen=github.com/kubeflow/kubeflow/components/poddefaults-webhooks/pkg/apis/settings
+// +k8s:conversion-gen=github.com/kubeflow/dashboard/components/poddefaults-webhooks/pkg/apis/settings
 // +k8s:defaulter-gen=TypeMeta
 // +groupName="kubeflow.org"
 package v1alpha1
