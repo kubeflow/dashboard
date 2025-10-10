91684f2cedf6c645d45c39d1bce20698a591cd85
Harshvir Potpose <hpotpose62@gmail.com>
2025-10-10 20:33:27 +0530

    fix install scripts
    Signed-off-by: Harshvir Potpose <hpotpose62@gmail.com>

--- testing/shared/install_centraldashboard_angular.sh
+++ testing/shared/install_centraldashboard_angular.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-IMG="${IMG:-centraldashboard-angular}"
+IMG="${IMG:-dashboard-angular}"
 TAG="${TAG:-integration-test}"
 
 ./testing/shared/deploy_component.sh components/centraldashboard-angular "${IMG}" "${TAG}" manifests overlays/istio
 
-kubectl wait --for=condition=Ready pods -n kubeflow -l app=centraldashboard-angular --timeout=300s
-kubectl wait --for=condition=Available deployment -n kubeflow centraldashboard-angular --timeout=300s
+kubectl wait --for=condition=Ready pods -n kubeflow -l app=dashboard-angular --timeout=300s
+kubectl wait --for=condition=Available deployment -n kubeflow dashboard-angular --timeout=300s
